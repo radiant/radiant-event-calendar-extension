3863383aaf3bb300753434e25dc03404db7ad8ef
spanner <will@spanner.org>
2010-04-11 16:30:54 +0100

    fixed remove links

--- event_calendar_extension.rb
+++ event_calendar_extension.rb
@@ -9,8 +9,8 @@ class EventCalendarExtension < Radiant::Extension
     map.namespace :admin, :path_prefix => EXT_ROOT do |cal|
       cal.resources :calendars
       cal.resources :icals, :collection => {:refresh_all => :any}, :member => {:refresh => :put}
-      cal.resources :events, :member => {:remove => :post}
-      cal.resources :event_venues, :member => {:remove => :post}
+      cal.resources :events, :member => {:remove => :get}
+      cal.resources :event_venues, :member => {:remove => :get}
       cal.calendars_home '/', :controller => 'events', :action => 'index'
     end
     map.calendar "/calendar.:format", :controller => 'events', :action => 'index'
