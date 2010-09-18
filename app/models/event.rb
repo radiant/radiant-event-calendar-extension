a1b51d20dde8781d0555dc2d2d5c4525b9b0dc61
Kevin Triplett <kevin@mopacmedia.com>
2010-09-18 13:00:14 -0500

    fixed postgres error: Boolean compared to integer in named scope

--- app/models/event.rb
+++ app/models/event.rb
@@ -10,10 +10,10 @@ class Event < ActiveRecord::Base
 
   belongs_to :event_venue
   accepts_nested_attributes_for :event_venue, :reject_if => proc { |attributes| attributes.all? {|k,v| v.blank?} } # radiant 0.8.1 is using rails 2.3.4, which doesn't include the :all_blank sugar
-  
+
   belongs_to :master, :class_name => 'Event'
   has_many :occurrences, :class_name => 'Event', :foreign_key => 'master_id', :dependent => :destroy
-  has_many :recurrence_rules, :class_name => 'EventRecurrenceRule', :dependent => :destroy, :conditions => {:active => 1}
+  has_many :recurrence_rules, :class_name => 'EventRecurrenceRule', :dependent => :destroy, :conditions => {:active => true}
   accepts_nested_attributes_for :recurrence_rules, :allow_destroy => true, :reject_if => lambda { |attributes| attributes['active'].to_s != '1' }
 
   validates_presence_of :uuid, :title, :start_date, :status_id
@@ -22,14 +22,14 @@ class Event < ActiveRecord::Base
   before_validation :set_uuid
   before_validation :set_default_status
   after_save :update_occurrences
-  
+
   default_scope :order => 'start_date ASC', :include => :event_venue
   named_scope :imported, { :conditions => ["status_id = ?", Status[:imported].to_s] }
   named_scope :submitted, { :conditions => ["status_id = ?", Status[:submitted].to_s] }
   named_scope :approved, { :conditions => ["status_id >= (?)", Status[:published].to_s] }
   named_scope :primary, { :conditions => "master_id IS NULL" }
   named_scope :recurrent, { :conditions => "master_id IS NOT NULL" }
-  
+
   named_scope :in_calendars, lambda { |calendars| # list of calendar objects
     ids = calendars.map{ |c| c.id }
     { :conditions => [ ids.map{"calendar_id = ?"}.join(" OR "), *ids] }
@@ -38,11 +38,11 @@ class Event < ActiveRecord::Base
   named_scope :after, lambda { |datetime| # datetime. eg calendar.occurrences.after(Time.now)
     { :conditions => ['start_date > ?', datetime] }
   }
-  
+
   named_scope :before, lambda { |datetime| # datetime. eg calendar.occurrences.before(Time.now)
     { :conditions => ['start_date < ?', datetime] }
   }
-  
+
   named_scope :within, lambda { |period| # CalendarPeriod object
     { :conditions => ['start_date > :start AND start_date < :finish', {:start => period.start, :finish => period.finish}] }
   }
@@ -50,15 +50,15 @@ class Event < ActiveRecord::Base
   named_scope :between, lambda { |start, finish| # datetimable objects. eg. Event.between(reader.last_login, Time.now)
     { :conditions => ['start_date > :start AND start_date < :finish', {:start => start, :finish => finish}] }
   }
-  
+
   named_scope :future_and_current, {
     :conditions => ['(end_date > :now) OR (end_date IS NULL AND start_date > :now)', {:now => Time.now}]
   }
-  
+
   named_scope :unfinished, lambda { |start| # datetimable object.
     { :conditions => ['start_date < :start AND end_date > :start', {:start => start}] }
   }
-  
+
   named_scope :coincident_with, lambda { |start, finish| # datetimable objects.
     { :conditions => ['(start_date < :finish AND end_date > :start) OR (end_date IS NULL AND start_date > :start AND start_date < :finish)', {:start => start, :finish => finish}] }
   }
@@ -82,13 +82,13 @@ class Event < ActiveRecord::Base
     finish = start + 1.month
     between(start, finish)
   end
-  
+
   def self.in_week(year, week)            # numbers, with a commercial week: eg calendar.occurrences.in_week(2010, 35)
     start = DateTime.commercial(year, week)
     finish = start + 1.week
     between(start, finish)
   end
-  
+
   def self.on_day (year, month, day)      # numbers: eg calendar.occurrences.on_day(2010, 12, 12)
     start = DateTime.civil(year, month, day)
     finish = start + 1.day
@@ -122,7 +122,7 @@ class Event < ActiveRecord::Base
   def slug
     calendar.slug if calendar
   end
-  
+
   def description_paragraph
     if description =~ /\<p/
       description
@@ -130,7 +130,7 @@ class Event < ActiveRecord::Base
       "<p>#{description}</p>"
     end
   end
-  
+
   def short_description(length=256, ellipsis="...")
     truncate(description, length, ellipsis)
   end
@@ -138,7 +138,7 @@ class Event < ActiveRecord::Base
   def master?
     master.nil?
   end
-  
+
   def occurrence?
     !master?
   end
@@ -146,7 +146,7 @@ class Event < ActiveRecord::Base
   def location
     event_venue ? event_venue.to_s : read_attribute(:location)
   end
-  
+
   def address
     event_venue ? event_venue.address : read_attribute(:location)
   end
@@ -158,7 +158,7 @@ class Event < ActiveRecord::Base
   def date
     start_date.to_datetime.strftime(date_format)
   end
-  
+
   def month
     Date::MONTHNAMES[start_date.month]
   end
@@ -178,11 +178,11 @@ class Event < ActiveRecord::Base
   def mday
     start_date.mday
   end
-  
+
   def short_date
     start_date.to_datetime.strftime(short_date_format)
   end
-  
+
   def start_time
     start_date.to_datetime.strftime(start_date.min == 0 ? round_time_format : time_format).downcase
   end
@@ -190,11 +190,11 @@ class Event < ActiveRecord::Base
   def end_time
     end_date.to_datetime.strftime(end_date.min == 0 ? round_time_format : time_format).downcase if end_date
   end
-  
+
   def last_day
     end_date.to_datetime.strftime(date_format)if end_date
   end
-  
+
   def duration
     if end_date
       end_date - start_date
@@ -210,7 +210,7 @@ class Event < ActiveRecord::Base
       start_time
     end
   end
-  
+
   def finishes
     if all_day?
       "all day"
@@ -228,7 +228,7 @@ class Event < ActiveRecord::Base
       "#{start_time} on #{date}"
     end
   end
-  
+
   def summarize_period
     period = []
     if one_day?
@@ -245,7 +245,7 @@ class Event < ActiveRecord::Base
     end
     period.join(' ')
   end
-  
+
   def url
     if url = read_attribute(:url)
       return nil if url.blank?
@@ -253,19 +253,19 @@ class Event < ActiveRecord::Base
       url.strip
     end
   end
-  
+
   def one_day?
     all_day? && within_day?
   end
-  
+
   def within_day?
     (!end_date || start_date.to_date.jd == end_date.to_date.jd || end_date == start_date + 1.day)
   end
-  
+
   # sometimes we need to filter an existing list to get the day's events
   # usually in a radius tag, to avoid going back to the database for each day in a list
   # so we call events.select {|e| e.on_this_day?(day) }
-  
+
   def on_this_day?(date)
     if end_date
       start_date < date.end_of_day && end_date > date.beginning_of_day
@@ -281,23 +281,23 @@ class Event < ActiveRecord::Base
       start_date > date.beginning_of_month && start_date < date.end_of_month
     end
   end
-  
+
   def continuing?
     end_date && start_date < Time.now && end_date > Time.now
   end
-  
+
   def editable?
     status != Status[:imported]
   end
-  
+
   def recurs?
     master || occurrences.any?
   end
-  
+
   def status
     Status.find(self.status_id)
   end
-  
+
   def status=(value)
     self.status_id = value.id
   end
@@ -305,11 +305,11 @@ class Event < ActiveRecord::Base
   def recurrence
     recurrence_rules.first.to_s
   end
-  
+
   def add_recurrence(rule)
     self.recurrence_rules << EventRecurrenceRule.from(rule)
   end
-      
+
   def to_ri_cal
     RiCal.Event do |cal_event|
       cal_event.uid = uuid
@@ -322,11 +322,11 @@ class Event < ActiveRecord::Base
       cal_event.location = location if location
     end
   end
-  
+
   def ical
     self.to_ri_cal.to_s
   end
-  
+
   def self.create_from(cal_event)
     event = new({
       :uuid => cal_event.uid,
@@ -346,7 +346,7 @@ class Event < ActiveRecord::Base
     logger.error "Event import error: #{error}."
     raise
   end
-  
+
   def update_from(cal_event)
     self.update_attributes({
       :title => cal_event.summary,
@@ -364,7 +364,7 @@ class Event < ActiveRecord::Base
     logger.error "Event update error: #{error}."
     raise
   end
-  
+
 protected
 
   def set_uuid
@@ -385,25 +385,25 @@ protected
       end
     end
   end
-    
+
   def date_format
     Radiant::Config['event_calendar.date_format'] || "%-1d %B"
   end
-  
+
   def short_date_format
     Radiant::Config['event_calendar.short_date_format'] || "%-1d/%m/%Y"
   end
-  
+
   def time_format
     Radiant::Config['event_calendar.time_format'] || "%-1I:%M%p"
   end
-  
+
   def round_time_format
     Radiant::Config['event_calendar.round_time_format'] || "%-1I%p"
   end
-  
+
   def dates_are_in_order
-    
+
   end
 
 end
